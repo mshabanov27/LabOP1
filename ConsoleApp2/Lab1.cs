6a5730b4e3007dc8b7309b762f250969e3b247ec
Mitya <mityashabanov2004@gmail.com>
2021-10-05 18:08:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Lab1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Введіть меншу основу: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введіть більшу основу: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введіть висоту: ");
+            double h = double.Parse(Console.ReadLine());
+            double S = (a + b)*h/2;
+            double c = Math.Sqrt(((b - a)*(b - a) / 4) + h*h);
+            double P = a + b + 2 * c;
+            Console.WriteLine("Площа трапеції дорівнює " + S);
+            Console.WriteLine("Периметр трапеції дорівнює " + P);
+        }
+    }
+}
---
+++ Lab2/Lab2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Lab2
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Введіть число k: ");
+            int k = int.Parse(Console.ReadLine());
+            int num;
+            if (k % 2 == 1)
+            {
+                num = k / 20 + 1;
+                Console.WriteLine("Цифра дорівнює: " + num);
+            }
+            else if (k % 2 == 0)
+            {
+                num = ((k / 2) - 1) % 10;
+                Console.WriteLine("Цифра дорівнює: " + num);
+            }
+        }
+    }
+}
